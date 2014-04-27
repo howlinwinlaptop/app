e63f875bbbec79c3c96d9202562b4bcd61f87a54
Cathy Howlin <cathyh07@gmail.com>
2014-04-27 14:25:51 +0100

    login update 4

--- app/app/controllers/Login.java
+++ app/app/controllers/Login.java
@@ -12,5 +12,33 @@ public class Login extends Controller {
     public static void index() {
         render();
     }
+    /**
+     * Authenticates that a user's login credentials are valid
+     * 
+     * @param email
+     *          The submitted email address
+     * @param password
+     *          The corresponding password
+     */
+    public static void authenticate(String email, String password)
+    {
+  	  
+      User user = User.findByEmail(email);
+      if ((user != null) && (user.checkPassword(password) == true))
+      {
+        Logger.info("Authentication successful");
+        session.put("logged_in_userid", user.id);
+        index();
+      }
+      else
+      {
+        Logger.info("Authentication failed");
+        login();
+      }
+    }
 
+    public static void login()
+    {
+      render();
+    }
 }
